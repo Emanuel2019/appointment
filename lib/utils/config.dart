a5ff0c5234e156e2cfceb5cb0032cc84c7a1fa31
NovatronicaST-DEV <emanuelngola33@gmail.com>
2023-02-18 11:49:48 +0100

    firts appointmente

---
+++ lib/utils/config.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static MediaQueryData? mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  void init(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    screenHeight = mediaQueryData!.size.height;
+    screenWidth = mediaQueryData!.size.width;
+  }
+
+  static get widthSize {
+    return screenWidth;
+  }
+
+  static get heightSize {
+    return screenHeight;
+  }
+
+  static const spaceSmall = SizedBox(
+    height: 25,
+  );
+  static final spaceMedium = SizedBox(
+    height: screenHeight! * 0.05,
+  );
+  
+}
